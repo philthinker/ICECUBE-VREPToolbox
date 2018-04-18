a55d1b7510c8ce9d541ca5f1a4dc5115fb11cb4b
Haopeng <philthinker@sina.com>
2018-04-18 21:40:37 +0800

    rmlMoveToPosition V-REP test done

--- Lua/UR5_IKRemote.lua
+++ Lua/UR5_IKRemote.lua
@@ -38,12 +38,22 @@ rem_rmlMoveToJointPositions = function(inInts,inFloats,inStrings,inBuffer)
 end
 
 rem_rmlMoveToPosition = function(inInts,inFloats,inStrings,inBuffer)
+    local targetPos = sim.getObjectPosition(ikTip, -1)
+    local targetQua = sim.getObjectQuaternion(ikTip, -1)
+    if #inFloats>=7 then
+        for i = 1,3,1 do
+            targetPos[i] = inFloats[i]
+        end
+        for i = 1,4,1 do
+            targetQua[i] = inFloats[i+3]
+        end
+    end
     if sim.getIntegerSignal('IKEnable') ~= 1 then
         enableIk(true)
         sim.setIntegerSignal('IKEnable', 1)
     end
-    if sim.getSimulationState ~= sim.simulation_advancing_abouttostop then
-        --
+    if sim.getSimulationState() ~= sim.simulation_advancing_abouttostop then
+        sim.rmlMoveToPosition(ikTarget, -1, -1, nil, nil, ikMaxVel, ikMaxAccel, ikMaxJerk, targetPos, targetQua, nil)
     end
     return {},{},{},''
 end
@@ -78,47 +88,31 @@ function sysCall_threadmain(  )
     ikMaxJerk={0.6,0.6,0.6,0.8}
 
     initialConfig={0,0,0,0,0,0}
-    startConfig={-70.1*math.pi/180,18.85*math.pi/180,93.18*math.pi/180,68.02*math.pi/180,109.9*math.pi/180,90*math.pi/180}
+    
 
     sim.setIntegerSignal('IKEnable', 0)         -- The sign for ik mechanism
     sim.addStatusbarMessage('The UR5 is ready to move!')
     --sim.setIntegerSignal('ClientRunning', 1)    -- The sign for remote client state
     
+    startConfig={-70.1*math.pi/180,18.85*math.pi/180,93.18*math.pi/180,68.02*math.pi/180,109.9*math.pi/180,90*math.pi/180}
 
     --rem_rmlMoveToJointPositions({},startConfig,{},'')
     --sim.wait(1)
     --rem_rmlMoveToJointPositions({},initialConfig,{},'')
     --sim.wait(1)
---[[
-    while sim.getIntegerSignal('ClientRunning') ~= 0 do
+
+    rem_rmlMoveToJointPositions({},startConfig,{},'')
+    local temp_pos = sim.getObjectPosition(ikTip, -1)
+    local temp_qua = sim.getObjectQuaternion(ikTip, -1)
+    rem_rmlMoveToPosition({},{temp_pos[1], temp_pos[2]-0.2, temp_pos[3],temp_qua[1], temp_qua[2], temp_qua[3], temp_qua[4]},{},'')
+    sim.wait(1)
+    rem_rmlMoveToPosition({},{temp_pos[1], temp_pos[2]-0.2, temp_pos[3]+0.2,temp_qua[1], temp_qua[2], temp_qua[3], temp_qua[4]},{},'')
+    sim.wait(1)
+    rem_rmlMoveToJointPositions({},initialConfig,{},'')
+
+    --while true do
         -- Just hold the thread
-        sim.wait(2)
-    end
-]]
-    while true do
-        -- Just hold the thread
-    end
---[[
-    if sim.getSimulationState()~=sim.simulation_advancing_abouttostop then
-        enableIk(true)
-
-        pos=sim.getObjectPosition(ikTip,-1)
-        quat=sim.getObjectQuaternion(ikTip,-1)
-        sim.rmlMoveToPosition(ikTarget,-1,-1,nil,nil,ikMaxVel,ikMaxAccel,ikMaxJerk,{pos[1],pos[2]-0.2,pos[3]},quat,nil)
-    end
-
-    if sim.getSimulationState()~=sim.simulation_advancing_abouttostop then
-        sim.wait(1)
-    end
-
-    if sim.getSimulationState()~=sim.simulation_advancing_abouttostop then
-        sim.rmlMoveToPosition(ikTarget,-1,-1,nil,nil,ikMaxVel,ikMaxAccel,ikMaxJerk,{pos[1],pos[2]-0.2,pos[3]+0.2},quat,nil)
-    end
-
-    if sim.getSimulationState()~=sim.simulation_advancing_abouttostop then
-        sim.wait(1)
-    end
-]]
+    --end
 
     sim.stopSimulation()
 end
