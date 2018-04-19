6c8d3b5e9435d599a0485a8f6e4516ae6c202559
Haopeng <philthinker@sina.com>
2018-04-19 12:43:40 +0800

    PIck-and-Place embedded demo done

---
+++ Lua/RG2_Close_Embedded.lua
@@ -0,0 +1,35 @@
+-- Non Threaded Child Script
+
+-- Attach these scripts to RG2 in V-REP scene "UR5plusRG2_PickAndPlace.ttt"
+-- Use these scripts for the Pick-and-Place demo if you wanna use embedded scripts only
+
+rg2Close = function(inInts,inFloats,inStrings,inBuffer)
+    local v = -motorVelocity
+    sim.setJointForce(motorHandle,motorForce)
+    sim.setJointTargetVelocity(motorHandle,v)
+    return {},{v},{},''
+end
+
+rg2Open = function(inInts,inFloats,inStrings,inBuffer)
+    local v = motorVelocity
+    sim.setJointForce(motorHandle,motorForce)
+    sim.setJointTargetVelocity(motorHandle,v)
+    return {},{v},{},''
+end
+
+function sysCall_init( )
+    motorHandle=sim.getObjectHandle('RG2_openCloseJoint')
+    motorVelocity=0.05 -- m/s
+    motorForce=20 -- N
+    sim.setIntegerSignal('RG2Close', 0)
+end
+
+function sysCall_actuation( )
+    -- Hear from UR5_PickAndPlace_Demo
+    local rg2Sign = sim.getIntegerSignal('RG2Close')
+    if rg2Sign ~= 0 then
+        rg2Close({}, {}, {}, '')
+    else
+        rg2Open({}, {}, {}, '')
+    end
+end
