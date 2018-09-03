8a518da2c50e3f66c27ff1fd524cb59ff86673fc
Haopeng <philthinker@sina.com>
2018-09-03 21:53:02 +0800

    v232 released

---
+++ robotTools/UR5VREPTools/ur5SetToJointPosition.m
@@ -0,0 +1,17 @@
+function [  ] = ur5SetToJointPosition( icecube, targetPositions )
+%ur5SetToJointPosition Set the UR5 to targetPositions
+% There is NO motion plan.
+% This is a ONE-SHOT function, DO NOT get something right after running it.
+% icecube: the ICECUBE object
+% targetPositions: 1 x 6 vector, the target joint positions
+
+icecube.vrep.simxPauseCommunication(icecube.clientID, 1);
+
+for i = 1:6
+    icecube.vrep.simxSetJointTargetPosition(icecube.clientID,icecube.handles.ur5Joints(i), targetPositions(i),icecube.vrep.simx_opmode_oneshot);
+end
+
+icecube.vrep.simxPauseCommunication(icecube.clientID, 0);
+
+end
+
