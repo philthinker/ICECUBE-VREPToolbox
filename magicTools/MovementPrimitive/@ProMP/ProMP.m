76d52f394c50aed4a9dbd305179b931a748f7642
Haopeng <philthinker@sina.com>
2018-12-28 09:49:26 +0800

    A short Gap for dSPACE

---
+++ magicTools/MovementPrimitive/@ProMP/ProMP.m
@@ -0,0 +1,12 @@
+classdef ProMP
+    %ProMP Probabilisitc Movement Primitive
+    % A. Paraschos, et al. Probabilisitc Movement Primitives. NIPS, 2013.
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
